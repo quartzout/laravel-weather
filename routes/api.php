d6f8f40cfb3c411ee8b661e2c2d6b39a4e3e73fb
quartzout <romatoid@gmail.com>
2022-12-06 18:03:39 +0300

    renamed api routes for consistency

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
 use App\Http\Controllers\ForecastByCityAndDateController;
+use App\Http\Controllers\ForecastByIdController;
 use App\Http\Controllers\ForecastController;
-use App\Http\Controllers\RecordsController;
-use App\Models\Record;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,17 +18,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResource("forecasts", ForecastController::class);
 
-Route::prefix("forecastsByCity/{cityName}")->group(function () {
+Route::prefix("forecasts-by-city-date/{cityName}")->group(function () {
 
-    Route::get("/", [ForecastByCityAndDateController::class, "index"]);
-    Route::get("{date}", [ForecastByCityAndDateController::class, "show"]);
-    Route::delete("{date}", [ForecastByCityAndDateController::class, "destroy"]);
+    Route::get("/", [ForecastByCityAndDateController::class, "index"])->name("forecasts-by-city-name.index");
+    Route::get("{date}", [ForecastByCityAndDateController::class, "show"])->name("forecasts-by-city-name.show");
+    Route::delete("{date}", [ForecastByCityAndDateController::class, "destroy"])->name("forecasts-by-city-name.destroy");
 
 });
 
